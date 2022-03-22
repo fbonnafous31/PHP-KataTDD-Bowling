5c9e9aadde6b87985c6db3514425bef66be19837
fbonnafous31 <francois.bonnafous@hotmail.fr>
2022-03-22 19:08:32 +0100

    fix spare

--- src/Bowling.php
+++ src/Bowling.php
@@ -11,32 +11,17 @@
         public function result(string $frame): int {
             $result = 0;
             for ($i=0; $i<strlen($frame); $i++) {
-                if ($i > 1) {
-                    // spare
-                    if ($frame[$i-1] == '/') {
-                        $cumul = 0;
-                    } else {
-                        $cumul = $frame[$i];
-                    }
-                } else {
-                    $cumul = $frame[$i];
-                }
-            
+                $cumul = $frame[$i];
+                if (($frame[$i-1] == '/') and ($i+1==strlen($frame))) $cumul = 0;
+                
                 // Miss 
                 if ($frame[$i] == '-') $cumul = 0;
 
                 // Spare
                 if ($frame[$i] == '/') {
                     // frame courant égal 10
-                    $cumul  = 10 - $frame[$i-1];   
-                    // double la valeur du premier lancer après spare
-                    if ($i+1<strlen($frame)) {
-                        $cumul += $frame[$i+1]*2;  
-                    }                   
+                    $cumul  = 10 - $frame[$i-1] + $frame[$i+1];                    
                 }
-                
-                // Strike
-                if ($frame[$i] == 'X') $cumul = 10;                        
                 $result += $cumul;
             }
             return $result;
--- tests/BowlingTest.php
+++ tests/BowlingTest.php
@@ -62,17 +62,6 @@ use function PHPUnit\Framework\assertEquals;
             assertEquals(0, $this->bowling->result($roll));
         }
 
-        public function testResultFrameWithSpare() {
-            $roll  = $this->bowling->roll('1');
-            $roll .= $this->bowling->roll('/');
-            assertEquals(10, $this->bowling->result($roll));
-        } 
-
-        public function testResultFrameWithStrike() {
-            $roll  = $this->bowling->roll('X');
-            assertEquals(10, $this->bowling->result($roll));
-        }
-
 
         // Teste le résultat de plusieurs frame (2 lancers)
         public function testResultFrame1111() {
@@ -87,10 +76,29 @@ use function PHPUnit\Framework\assertEquals;
             $roll  = $this->bowling->roll('1');
             $roll .= $this->bowling->roll('/');
             $roll .= $this->bowling->roll('1');
-            $roll .= $this->bowling->roll('1');
-            assertEquals(13, $this->bowling->result($roll));
+            assertEquals(11, $this->bowling->result($roll));
         } 
 
+        // 9- 9- 9- 9- 9- 9- 9- 9- 9- 9- (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90
+        public function testResultFor10PairsOf9andMiss() {
+            $roll = '';
+            for ($i=0; $i<10; $i++) {
+                $roll .= $this->bowling->roll('9');
+                $roll .= $this->bowling->roll('-');
+            }
+            assertEquals('90', $this->bowling->result($roll));
+        }
+
+        // 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150       
+        public function testResultFor10PairsOf5andSpare() {
+            $roll = '';
+            for ($i=0; $i<10; $i++) {
+                $roll .= $this->bowling->roll('5');
+                $roll .= $this->bowling->roll('/');
+            }
+            $roll .= $this->bowling->roll('5');
+            assertEquals('150', $this->bowling->result($roll));
+        }
     }
 
 ?>
