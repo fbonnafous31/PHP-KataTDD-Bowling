bcd574a7c01e89299455ae0d8e8a16fb56bb44fb
fbonnafous31 <francois.bonnafous@hotmail.fr>
2022-03-25 17:06:10 +0100

    frame management fix bugs

--- src/Bowling.php
+++ src/Bowling.php
@@ -8,22 +8,96 @@
             return $value;
         }
 
-        public function result(string $frame): int {
-            $result = 0;
-            for ($i=0; $i<strlen($frame); $i++) {
-                $cumul = $frame[$i];
-                if (($frame[$i-1] == '/') and ($i+1==strlen($frame))) $cumul = 0;
-                
-                // Miss 
-                if ($frame[$i] == '-') $cumul = 0;
-
-                // Spare
-                if ($frame[$i] == '/') {
-                    // frame courant égal 10
-                    $cumul  = 10 - $frame[$i-1] + $frame[$i+1];                    
+        public function frame_result(string $frame): int {
+            $result    = 0;
+            for ($i=0; $i<strlen($frame); $i++) {  
+                $next1     = 0;
+                $next2     = 0;    
+                // previous ball    
+                if ($i > 0) {
+                    $previous = $frame[$i-1];
+                    if ($previous == '-') $previous = 0;
                 }
-                $result += $cumul;
+
+                // next balls
+                if ($i+1 < strlen($frame)) $next1 = $frame[$i+1];
+                if (isset($next1) and ($next1 === '-')) $next1 = 0;
+                if (isset($next1) and ($next1 === 'X')) $next1 = 10;
+                if ($i+2 < strlen($frame)) $next2 = intval($frame[$i+2]);
+                if (isset($next2)) {
+                   if ($next2 == '-') $next2 = 0;
+                   if ($next2 == '/') $next2 = 10;
+                } 
+
+                // current ball
+                if (($frame[$i] != '/') or ($frame[$i] != '/') or ($frame[$i] != '/')) {
+                    $score = intval($frame[$i]);   
+                }
+                
+                // exception ball
+                if ($frame[$i] === '-') $score = 0; 
+                if ($frame[$i] === '/') $score = 10;
+                if ($frame[$i] === 'X') $score = 10;
+
+                // spare (/)
+                if ($frame[$i] == '/') {
+                    $score -= $previous;
+
+                    // exception last ball
+                    if ($i+2 >= strlen($frame)) $score += 0;
+                    else $score += $next1;
+                }
+
+                // strike (X)
+                if ($frame[$i] == 'X') {
+                    if ((isset($previous)) and ($previous = 'X')) $score += 10; 
+                    $score += $next1 + $next2;
+                }  
+
+                $result += $score;
+
+                if ($frame[$i] == 'X') return $result; 
             }
+            return $result;
+        }
+
+        public function game_result(): int {
+            $frames   = func_get_args();
+            $nbframes = func_num_args();
+            $result   = 0;
+            $i        = 0;
+
+            foreach ($frames as $frame) {
+
+                if (isset($frame[1]) and ($frame[1] == '/')) {
+                    if ($i+1<$nbframes) $frame .= $frames[$i+1][0];
+                }
+
+                if ($frame[0] == 'X') {
+                    if ($i+1<$nbframes) {
+                        if ($frames[$i+1][0] == 'X') {
+                            $frame .= 'X';
+                            if (isset($frames[$i+2][0]) and ($frames[$i+2][0]) == 'X') $frame .= 'X';
+                            else {
+                                if (isset($frames[$i+2][0])) $frame .= $frames[$i+2][0];
+                            }     
+                        }
+                        else {
+                            $frame .= $frames[$i+1][0];
+                            $frame .= $frames[$i+1][1];
+                        } 
+                    } 
+                }
+
+                if (($frame === 'XXX') and ($i+3 >= $nbframes)) $result -=20;
+                if (substr($frame, 0, 2)=='XX') {
+                    if (strlen($frame)==3) {
+                        $result += $this->frame_result($frame);
+                    }
+                } else $result += $this->frame_result($frame);
+                $i++;
+            }
+
             return $result;
         }
 
--- tests/BowlingTest.php
+++ tests/BowlingTest.php
@@ -42,63 +42,211 @@ use function PHPUnit\Framework\assertEquals;
             assertEquals('X', $roll);
         }
 
-
         // Teste le résultat d'un frame (2 lancers)
         public function testResultFrame11() {
             $roll  = $this->bowling->roll('1');
             $roll .= $this->bowling->roll('1');
-            assertEquals(2, $this->bowling->result($roll));
+            assertEquals(2, $this->bowling->frame_result($roll));
         }
 
         public function testResultFrameOneMiss() {
             $roll  = $this->bowling->roll('1');
             $roll .= $this->bowling->roll('-');
-            assertEquals(1, $this->bowling->result($roll));
+            assertEquals(1, $this->bowling->frame_result($roll));
         }
 
         public function testResultFrameTwoMiss() {
             $roll  = $this->bowling->roll('-');
             $roll .= $this->bowling->roll('-');
-            assertEquals(0, $this->bowling->result($roll));
-        }
-
-
-        // Teste le résultat de plusieurs frame (2 lancers)
-        public function testResultFrame1111() {
-            $roll  = $this->bowling->roll('1');
-            $roll .= $this->bowling->roll('1');
-            $roll .= $this->bowling->roll('1');
-            $roll .= $this->bowling->roll('1');
-            assertEquals(4, $this->bowling->result($roll));
+            assertEquals(0, $this->bowling->frame_result($roll));
         }
 
         public function testResultFramesWithSpare() {
             $roll  = $this->bowling->roll('1');
             $roll .= $this->bowling->roll('/');
+            $roll .= $this->bowling->roll('4');
+            assertEquals(14, $this->bowling->frame_result($roll));
+        } 
+
+        public function testResultFramesWithStrike() {
+            $roll  = $this->bowling->roll('X');
+            $roll .= $this->bowling->roll('8');
             $roll .= $this->bowling->roll('1');
-            assertEquals(11, $this->bowling->result($roll));
+            assertEquals(19, $this->bowling->frame_result($roll));
+        } 
+
+        // Test de plusieurs frame
+        public function testResultTwoFrames1111() {
+            $frame1  = $this->bowling->roll('1');
+            $frame1 .= $this->bowling->roll('1');
+            $frame2  = $this->bowling->roll('1');
+            $frame2 .= $this->bowling->roll('1');
+            assertEquals(4, $this->bowling->game_result($frame1, $frame2));
+        }
+
+        public function testResultFrameWithOneSpare() {
+            $ball  = $this->bowling->roll('2');
+            $ball .= $this->bowling->roll('/');
+            $ball .= $this->bowling->roll('4');
+            assertEquals(14, $this->bowling->frame_result($ball));
+        } 
+
+        public function testResultFrameWithOneStrike() {
+            $ball  = $this->bowling->roll('X');
+            $ball .= $this->bowling->roll('7');
+            $ball .= $this->bowling->roll('2');
+            assertEquals(19, $this->bowling->frame_result($ball));
+        } 
+
+        public function testResultFrameWithTwoStrike() {
+            $ball  = $this->bowling->roll('X');
+            $ball .= $this->bowling->roll('X');
+            $ball .= $this->bowling->roll('2');
+            assertEquals(22, $this->bowling->frame_result($ball));
+        } 
+
+        public function testResultFrameWithThreeStrike() {
+            $ball  = $this->bowling->roll('X');
+            $ball .= $this->bowling->roll('X');
+            $ball .= $this->bowling->roll('X');
+            assertEquals(30, $this->bowling->frame_result($ball));
+        } 
+
+        public function testResultFramesWithTwoSpare() {
+            $frame1  = $this->bowling->roll('5');
+            $frame1 .= $this->bowling->roll('/');
+            $frame2  = $this->bowling->roll('5');
+            $frame2 .= $this->bowling->roll('/');
+            $frame2 .= $this->bowling->roll('5');
+            assertEquals(30, $this->bowling->game_result($frame1, $frame2));
+        } 
+        
+        public function testResultFramesWithThreeStrike() {
+            $frame1 = $this->bowling->roll('X');
+            $frame2 = $this->bowling->roll('X');
+            $frame3 = $this->bowling->roll('X');
+            assertEquals(30, $this->bowling->game_result($frame1, $frame2, $frame3));
         } 
 
         // 9- 9- 9- 9- 9- 9- 9- 9- 9- 9- (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90
         public function testResultFor10PairsOf9andMiss() {
-            $roll = '';
-            for ($i=0; $i<10; $i++) {
-                $roll .= $this->bowling->roll('9');
-                $roll .= $this->bowling->roll('-');
-            }
-            assertEquals('90', $this->bowling->result($roll));
+            $frame1   = $this->bowling->roll('9');
+            $frame1  .= $this->bowling->roll('-');
+            $frame2   = $this->bowling->roll('9');
+            $frame2  .= $this->bowling->roll('-');
+            $frame3   = $this->bowling->roll('9');
+            $frame3  .= $this->bowling->roll('-');
+            $frame4   = $this->bowling->roll('9');
+            $frame4  .= $this->bowling->roll('-');
+            $frame5   = $this->bowling->roll('9');
+            $frame5  .= $this->bowling->roll('-');
+            $frame6   = $this->bowling->roll('9');
+            $frame6  .= $this->bowling->roll('-');
+            $frame7   = $this->bowling->roll('9');
+            $frame7  .= $this->bowling->roll('-');
+            $frame8   = $this->bowling->roll('9');
+            $frame8  .= $this->bowling->roll('-');
+            $frame9   = $this->bowling->roll('9');
+            $frame9  .= $this->bowling->roll('-');
+            $frame10  = $this->bowling->roll('9');
+            $frame10 .= $this->bowling->roll('-');
+
+            assertEquals('90', $this->bowling->game_result($frame1, $frame2, $frame3, $frame4, $frame5, $frame6, $frame7, $frame8, $frame9, $frame10));
         }
 
         // 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150       
         public function testResultFor10PairsOf5andSpare() {
-            $roll = '';
-            for ($i=0; $i<10; $i++) {
-                $roll .= $this->bowling->roll('5');
-                $roll .= $this->bowling->roll('/');
-            }
-            $roll .= $this->bowling->roll('5');
-            assertEquals('150', $this->bowling->result($roll));
+            $frame1  = $this->bowling->roll('5');
+            $frame1 .= $this->bowling->roll('/');
+            $frame2  = $this->bowling->roll('5');
+            $frame2 .= $this->bowling->roll('/');
+            $frame3  = $this->bowling->roll('5');
+            $frame3 .= $this->bowling->roll('/');
+            $frame4  = $this->bowling->roll('5');
+            $frame4 .= $this->bowling->roll('/');
+            $frame5  = $this->bowling->roll('5');
+            $frame5 .= $this->bowling->roll('/');
+            $frame6  = $this->bowling->roll('5');
+            $frame6 .= $this->bowling->roll('/');
+            $frame7  = $this->bowling->roll('5');
+            $frame7 .= $this->bowling->roll('/');
+            $frame8  = $this->bowling->roll('5');
+            $frame8 .= $this->bowling->roll('/');
+            $frame9  = $this->bowling->roll('5');
+            $frame9 .= $this->bowling->roll('/');
+            $frame10  = $this->bowling->roll('5');
+            $frame10 .= $this->bowling->roll('/');
+            $frame10 .= $this->bowling->roll('5');
+            assertEquals('150', $this->bowling->game_result($frame1, $frame2, $frame3, $frame4, $frame5, $frame6, $frame7, $frame8, $frame9, $frame10));
         }
+
+        // X X X X X X X X X X X X (12 rolls: 12 strikes) = 10 frames * 30 points = 300
+        public function testResultFor10Strike() {
+            $frame1   = $this->bowling->roll('X');
+            $frame2   = $this->bowling->roll('X');
+            $frame3   = $this->bowling->roll('X');
+            $frame4   = $this->bowling->roll('X');
+            $frame5   = $this->bowling->roll('X');
+            $frame6   = $this->bowling->roll('X');
+            $frame7   = $this->bowling->roll('X');
+            $frame8   = $this->bowling->roll('X');
+            $frame9   = $this->bowling->roll('X');
+            $frame10  = $this->bowling->roll('X');
+            $frame11  = $this->bowling->roll('X');
+            $frame12  = $this->bowling->roll('X');
+            assertEquals('300', $this->bowling->game_result($frame1, $frame2, $frame3, $frame4, $frame5, $frame6, $frame7, $frame8, $frame9, $frame10, $frame11, $frame12));
+        }
+
+        // Exemple de résultat de partie
+        // https://bcc85.fr/calcul-du-score/
+        public function testGame1() {
+            $frame1   = $this->bowling->roll('1');
+            $frame1  .= $this->bowling->roll('2');
+            $frame2   = $this->bowling->roll('X');
+            $frame3   = $this->bowling->roll('0');
+            $frame3  .= $this->bowling->roll('/');
+            $frame4   = $this->bowling->roll('4');
+            $frame4  .= $this->bowling->roll('2');
+            $frame5   = $this->bowling->roll('0');
+            $frame5  .= $this->bowling->roll('/');
+            $frame6   = $this->bowling->roll('6');
+            $frame6  .= $this->bowling->roll('2');
+            $frame7   = $this->bowling->roll('0');
+            $frame7  .= $this->bowling->roll('/');
+            $frame8   = $this->bowling->roll('6');
+            $frame8  .= $this->bowling->roll('/');
+            $frame9   = $this->bowling->roll('8');
+            $frame9  .= $this->bowling->roll('/');
+            $frame10  = $this->bowling->roll('2');
+            $frame10 .= $this->bowling->roll('/');
+            $frame10 .= $this->bowling->roll('8');
+        assertEquals(131, $this->bowling->game_result($frame1, $frame2, $frame3, $frame4, $frame5, $frame6, $frame7, $frame8, $frame9, $frame10));
+        }
+
+        public function testGame2() {
+            $frame1   = $this->bowling->roll('-');
+            $frame1  .= $this->bowling->roll('/');
+            $frame2   = $this->bowling->roll('1');
+            $frame2  .= $this->bowling->roll('7');
+            $frame3   = $this->bowling->roll('0');
+            $frame3  .= $this->bowling->roll('/');
+            $frame4   = $this->bowling->roll('0');
+            $frame4  .= $this->bowling->roll('2');
+            $frame5   = $this->bowling->roll('0');
+            $frame5  .= $this->bowling->roll('/');
+            $frame6   = $this->bowling->roll('6');
+            $frame6  .= $this->bowling->roll('0');
+            $frame7   = $this->bowling->roll('X');
+            $frame8   = $this->bowling->roll('0');
+            $frame8  .= $this->bowling->roll('/');
+            $frame9   = $this->bowling->roll('6');
+            $frame9  .= $this->bowling->roll('2');
+            $frame10  = $this->bowling->roll('0');
+            $frame10 .= $this->bowling->roll('/');
+            $frame10 .= $this->bowling->roll('2');
+            assertEquals(109, $this->bowling->game_result($frame1, $frame2, $frame3, $frame4, $frame5, $frame6, $frame7, $frame8, $frame9, $frame10));
+        }
+
     }
 
 ?>
